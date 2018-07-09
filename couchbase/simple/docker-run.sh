56853e6df510722df32c27ec7807abb9d54eb0e2
Maxime Beugnet <maxime.beugnet@gmail.com>
2018-07-09 14:00:38 +0200

    update couchbase to 5.1.0

--- couchbase/simple/docker-run.sh
+++ couchbase/simple/docker-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -d -h couchbase --name couchbase -p 8091-8094:8091-8094 -p 11210:11210 -v /etc/localtime:/etc/localtime:ro couchbase:4.6.3
+docker run -d -h couchbase --name couchbase -p 8091-8094:8091-8094 -p 11210:11210 -v /etc/localtime:/etc/localtime:ro couchbase:5.1.0
