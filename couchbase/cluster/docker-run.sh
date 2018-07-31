d478344b5ea9c93c70899a81d23e406a71197848
Maxime Beugnet <maxime.beugnet@gmail.com>
2018-07-31 11:18:24 +0200

    update couchbase version to 5.1.0 and added network

--- couchbase/cluster/docker-run.sh
+++ couchbase/cluster/docker-run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-docker run -d -h couchbase-cluster-1 --name couchbase-cluster-1 -p 7091-7094:8091-8094 -p 11210:11210 -v /etc/localtime:/etc/localtime:ro --network netcouchbase --network-alias couchbase-cluster-1 couchbase:4.6.3
-docker run -d -h couchbase-cluster-2 --name couchbase-cluster-2 -p 8091-8094:8091-8094 -p 21210:11210 -v /etc/localtime:/etc/localtime:ro --network netcouchbase --network-alias couchbase-cluster-2 couchbase:4.6.3
-docker run -d -h couchbase-cluster-3 --name couchbase-cluster-3 -p 9091-9094:8091-8094 -p 31210:11210 -v /etc/localtime:/etc/localtime:ro --network netcouchbase --network-alias couchbase-cluster-3 couchbase:4.6.3
+docker network create netcouchbase
+docker run -d -h couchbase-cluster-1 --name couchbase-cluster-1 -p 8091-8094:8091-8094 -p 11210:11210 -v /etc/localtime:/etc/localtime:ro --network netcouchbase --network-alias couchbase-cluster-1 couchbase:5.1.0
+docker run -d -h couchbase-cluster-2 --name couchbase-cluster-2 -p 8191-8194:8091-8094 -p 11211:11210 -v /etc/localtime:/etc/localtime:ro --network netcouchbase --network-alias couchbase-cluster-2 couchbase:5.1.0
+docker run -d -h couchbase-cluster-3 --name couchbase-cluster-3 -p 8291-8294:8091-8094 -p 11212:11210 -v /etc/localtime:/etc/localtime:ro --network netcouchbase --network-alias couchbase-cluster-3 couchbase:5.1.0
